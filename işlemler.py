dc845807404ee94f0585678f3ee9d95543a95132
MehmetDemr <demirmehmet226648@gmail.com>
2023-05-18 11:04:57 +0300

    Çarpma fonksiyonu eklendi

--- işlemler.py
+++ işlemler.py
@@ -1,5 +1,10 @@
 def toplama(a,b):
     return a+b
+
+def carpma(a,b):
+    return a*b
+
 x=int(input("Lütfen ilk sayıyı giriniz:"))
 y=int(input("Lütfen ikinci sayıyı giriniz:"))
 print(toplama(x,y))
+print(carpma(x,y))
