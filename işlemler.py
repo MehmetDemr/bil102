24f1d6def98361a842fc81cb9b8fec5f976b081f
MehmetDemr <demirmehmet226648@gmail.com>
2023-05-18 11:07:27 +0300

    Bölme fonksiyonu eklendi

--- işlemler.py
+++ işlemler.py
@@ -4,6 +4,13 @@ def toplama(a,b):
 def carpma(a,b):
     return a*b
 
+def bolme(a,b):
+    if(b>0):
+        return a/b
+    else:
+        print("Payda kısmına 0 gelemez!!!")
+        return False
+
 x=int(input("Lütfen ilk sayıyı giriniz:"))
 y=int(input("Lütfen ikinci sayıyı giriniz:"))
 print(toplama(x,y))
